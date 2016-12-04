7de81cc2800a73c7f62bdd33232ece532269b3f4
Vedmurty Chavan <ved1@umbc.edu>
2016-12-04 17:24:59 -0500

    Change output file name Add option "--disable-warnings" to nvcc

--- compileAndRunCuda.py
+++ compileAndRunCuda.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python
-
 import sys
 import os
 
-cudaCompiler = "nvcc"
-workLoadFile = sys.argv[2]
-resultFile   = sys.argv[4]
-exeName      = "runFile_" + workLoadFile.split("_")[1][:-3] + ".exe"
-cmdCompile = cudaCompiler + " " + workLoadFile + " -o " + exeName
+cudaCompiler      = "nvcc"
+cudaCompilerFlags = " --disable-warnings "
+workLoadFile      = sys.argv[2]
+resultFile        = sys.argv[4]
+exeName           = "runFile_" + workLoadFile.split("_")[1] + workLoadFile.split("_")[2][:-3] + ".exe"
+
+
+cmdCompile = cudaCompiler + cudaCompilerFlags + workLoadFile + " -o " + exeName
+
+print "COMPILATION COMMAND : ", cmdCompile
 
 os.system(cmdCompile)
 
